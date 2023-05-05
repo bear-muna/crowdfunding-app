c30e2dabd8c99306c53962c98cf345b164e95a1f
bear-muna <bearmuna@gmail.com>
2023-05-05 11:18:31 -0700

    connected to handlebars engine

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -17,6 +18,11 @@ const sess = {
     db: sequelize
   })
 };
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(session(sess));
 
